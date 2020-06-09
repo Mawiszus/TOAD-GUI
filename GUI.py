18ebc4e1aed1fbf3324f699c6277ec62ced2a269
Maren Awiszus <awiszus@tnt.uni-hannover.de>
2020-06-09 14:15:26 +0200

    Layout rework, splitted open level/generator to separate buttons, open generator now opens a directory.

--- GUI.py
+++ GUI.py
@@ -7,7 +7,7 @@ import os
 import multiprocessing as mp
 
 from utils.scrollable_image import ScrollableImage
-from utils.level_utils import read_level_from_file, one_hot_to_ascii_level
+from utils.level_utils import read_level_from_file, one_hot_to_ascii_level, place_a_mario_token
 from utils.level_image_gen import LevelImageGen
 from utils.toad_gan_utils import load_trained_pyramid, generate_sample, TOADGAN_obj
 
@@ -53,42 +53,57 @@ def TOAD_GUI():
     is_loaded.set(False)
 
     # Define Callbacks
-    def load():
-        fname = fd.askopenfilename(title='Save level', initialdir=os.curdir,
-                                   filetypes=[("level files", "*.txt *.pth")])
+    def load_level():
+        fname = fd.askopenfilename(title='Load Level', initialdir=os.curdir,
+                                   filetypes=[("level .txt files", "*.txt")])
         try:
             if fname[-3:] == "txt":
                 load_string_gen.set(fname)
                 folder, lname = os.path.split(fname)
 
                 lev, tok = read_level_from_file(folder, lname)
-                img = ImageTk.PhotoImage(ImgGen.render(one_hot_to_ascii_level(lev, tok)))
 
-                level_obj.image = img
                 level_obj.oh_level = lev
                 level_obj.ascii_level = one_hot_to_ascii_level(lev, tok)
+                m_exists = False
+                for line in level_obj.ascii_level:
+                    if 'M' in line:
+                        m_exists = True
+                if not m_exists:
+                    level_obj.ascii_level = place_a_mario_token(level_obj.ascii_level)
                 level_obj.tokens = tok
+
+                img = ImageTk.PhotoImage(ImgGen.render(level_obj.ascii_level))
+                level_obj.image = img
 
                 use_gen.set(False)
                 is_loaded.set(True)
-            elif fname[-3:] == "pth":
-                load_string_gen.set(fname)
-                folder, gname = os.path.split(fname)
-
-                loadgan, msg = load_trained_pyramid(folder)
-                toadgan_obj.Gs = loadgan.Gs
-                toadgan_obj.Zs = loadgan.Zs
-                toadgan_obj.reals = loadgan.reals
-                toadgan_obj.NoiseAmp = loadgan.NoiseAmp
-                toadgan_obj.token_list = loadgan.token_list
-                toadgan_obj.num_layers = loadgan.num_layers
-
-                error_msg.set(msg)
-
-                use_gen.set(True)
-                is_loaded.set(False)
             else:
-                error_msg.set("Can only load .txt levels or .pth pretrained Generators.")
+                error_msg.set("Can only load .txt levels.")
+        except Exception:
+            error_msg.set("Could not load generator/level. Is the filepath correct?")
+
+        return
+
+    def load_generator():
+        fname = fd.askdirectory(title='Load Generator Directory', initialdir=os.curdir)
+
+        try:
+            load_string_gen.set(fname)
+            folder = fname
+
+            loadgan, msg = load_trained_pyramid(folder)
+            toadgan_obj.Gs = loadgan.Gs
+            toadgan_obj.Zs = loadgan.Zs
+            toadgan_obj.reals = loadgan.reals
+            toadgan_obj.NoiseAmp = loadgan.NoiseAmp
+            toadgan_obj.token_list = loadgan.token_list
+            toadgan_obj.num_layers = loadgan.num_layers
+
+            error_msg.set(msg)
+
+            use_gen.set(True)
+            is_loaded.set(False)
 
         except Exception:
             error_msg.set("Could not load generator/level. Is the filepath correct?")
@@ -114,9 +129,15 @@ def TOAD_GUI():
                                     toadgan_obj.num_layers, toadgan_obj.token_list).cpu()
             level_obj.oh_level = level
             level_obj.ascii_level = one_hot_to_ascii_level(level, toadgan_obj.token_list)
+            m_exists = False
+            for line in level_obj.ascii_level:
+                if 'M' in line:
+                    m_exists = True
+            if not m_exists:
+                level_obj.ascii_level = place_a_mario_token(level_obj.ascii_level)
             level_obj.tokens = toadgan_obj.token_list
 
-            img = ImageTk.PhotoImage(ImgGen.render(one_hot_to_ascii_level(level, toadgan_obj.token_list)))
+            img = ImageTk.PhotoImage(ImgGen.render(level_obj.ascii_level))
 
             level_obj.image = img
 
@@ -135,6 +156,8 @@ def TOAD_GUI():
         game.window.dispose()
         # except Exception:
         #     error_msg.set("Java Mario Framework crashed. Please check your installation.")
+        # print(result.getCompletionPercentage())
+        error_msg.set("Level Played. Completion Percentage: %d%%" % int(result.getCompletionPercentage() * 100))
         return
 
     # Make layout
@@ -142,7 +165,8 @@ def TOAD_GUI():
 
     f_label = ttk.Label(settings, text='Path:')
     fpath_label = ttk.Label(settings, textvariable=load_string_gen, width=100)
-    load_button = ttk.Button(settings, text='Open', command=load)
+    load_lev_button = ttk.Button(settings, text='Open Level', command=load_level)
+    load_gen_button = ttk.Button(settings, text='Open Generator', command=load_generator)
 
     gen_button = ttk.Button(settings, text='Generate level', state='disabled', command=generate)
     save_button = ttk.Button(settings, text='Save level', state='disabled', command=save_txt)
@@ -177,14 +201,15 @@ def TOAD_GUI():
 
     # Pack layout to grid
     settings.grid(column=0, row=0, sticky=(N, S, E, W))
-    f_label.grid(column=0, row=0, sticky=(S, W), padx=5, pady=5)
+    f_label.grid(column=0, row=0, columnspan=4, sticky=(S, W), padx=5, pady=5)
     fpath_label.grid(column=0, row=1, columnspan=4, sticky=(N, W), padx=5, pady=5)
-    load_button.grid(column=0, row=2, sticky=(N, S, E, W), padx=5, pady=5)
-    gen_button.grid(column=1, row=2, sticky=(N, S, E, W), padx=5, pady=5)
-    save_button.grid(column=2, row=2, sticky=(N, S, E, W), padx=5, pady=5)
-    play_button.grid(column=3, row=2, sticky=(N, S, E, W), padx=5, pady=5)
-    prev_label.grid(column=0, row=3, sticky=(S, W), padx=5, pady=5)
-    image_label.grid(column=0, row=4, columnspan=4, sticky=(N, E, W), padx=5, pady=5)
+    load_lev_button.grid(column=1, row=2, sticky=(N, S, E, W), padx=5, pady=5)
+    load_gen_button.grid(column=2, row=2, sticky=(N, S, E, W), padx=5, pady=5)
+    gen_button.grid(column=1, row=3, sticky=(N, S, E, W), padx=5, pady=5)
+    save_button.grid(column=2, row=3, sticky=(N, S, E, W), padx=5, pady=5)
+    prev_label.grid(column=0, row=4, columnspan=4, sticky=(S, W), padx=5, pady=5)
+    image_label.grid(column=0, row=5, columnspan=4, sticky=(N, E, W), padx=5, pady=5)
+    play_button.grid(column=1, row=6, columnspan=2,  sticky=(N, S, E, W), padx=5, pady=5)
     error_label.grid(column=0, row=10, columnspan=4, sticky=(S, E, W), padx=5, pady=5)
 
     root.columnconfigure(0, weight=1)
@@ -197,7 +222,9 @@ def TOAD_GUI():
     settings.rowconfigure(1, weight=1)
     settings.rowconfigure(2, weight=1)
     settings.rowconfigure(3, weight=1)
-    settings.rowconfigure(4, weight=2)
+    settings.rowconfigure(4, weight=1)
+    settings.rowconfigure(5, weight=1)
+    settings.rowconfigure(6, weight=1)
     settings.rowconfigure(10, weight=1)
 
     # Run Window
